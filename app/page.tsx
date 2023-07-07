609534dc77c0cf6aca8ccf919b6c97ab80ca33d9
Jamey Hart <jamey.hart@jisc.ac.uk>
2023-07-07 13:40:03 +0100

    Gradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <>
-      Something...
-    </>
+    <div className="h-screen">
+        <div className="h-1/3 rounded-b-[24px] shadow flex justify-center bg-gradient-to-b from-indigo-600 to-indigo-700">
+          <p className="py-8 font-bold text-xl text-indigo-100/70">
+            Your Result
+          </p>
+        </div>
+      </div>
   )
 }
