1bf6642696b9282953a1648a0aaf6181da4f7712
Jamey Hart <jamey.hart@jisc.ac.uk>
2023-07-07 20:26:26 +0100

    Result section
    Mobile first!

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,33 @@
 export default function Home() {
     return (
         <div className="h-screen">
-            <div className="h-1/3 rounded-b-[24px] shadow bg-gradient-to-b from-indigo-600 to-indigo-700 py-20">
-                <div className="flex flex-col items-center justify-center h-full pb-12">
-                        <p className="py-8 font-semibold text-2xl text-indigo-100/70 leading-10">
-                            Your Result
-                        </p>
-                        {/* Result */}
+            <div className="h-4/9 rounded-b-[24px] shadow bg-gradient-to-b from-indigo-600 to-indigo-700">
+                <div className="flex flex-col items-center justify-center h-full">
+                    <p className="font-semibold text-xl text-gray-50/60 py-8">
+                        Your&nbsp;Result
+                    </p>
+
+                    {/* Result */}
+                    <div className="pb-8">
                         <span className="flex flex-col rounded-full p-8 px-10 items-center justify-center opacity-90 bg-gradient-to-b from-indigo-900 to-indigo-600">
                             <p className="text-white text-7xl font-extrabold">
                                 76
                             </p>
-                            <p className="font-bold text-indigo-100/70">
+                            <p className="font-bold text-gray-50/40">
                                 of 100
                             </p>
                         </span>
+                    </div>
+
+                    <p className="font-semibold text-2xl text-white pb-4">
+                        Great
+                    </p>
+
+                    <p className="px-10 text-center font-semibold text-lg text-gray-400 pb-10">
+                        You scored higher than 65% of the people who have taken these tests.
+                    </p>
                 </div>
             </div>
         </div>
-    )
+    );
 }
