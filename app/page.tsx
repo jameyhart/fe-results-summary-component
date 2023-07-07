02182bd3d4d40eb01f627092405503b06f6954bc
Jamey Hart <jamey.hart@jisc.ac.uk>
2023-07-07 20:53:01 +0100

    Not looking too bad?
    For a Friday night anyway! 😄

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div className="h-screen">
+        <div className="h-screen space-x-8 space-y-6">
             {/* Top section */}
             <div className="h-4/9 rounded-b-[24px] shadow bg-gradient-to-b from-indigo-600 to-indigo-700">
                 <div className="flex flex-col items-center justify-center h-full">
@@ -10,7 +10,7 @@ export default function Home() {
 
                     {/* Result */}
                     <div className="pb-8">
-                        <span className="flex flex-col rounded-full p-8 px-10 items-center justify-center opacity-90 bg-gradient-to-b from-indigo-900 to-indigo-600">
+                        <span className="flex flex-col rounded-full p-8 px-10 items-center justify-center opacity-90 bg-gradient-to-b from-indigo-800 to-indigo-600">
                             <p className="text-white text-7xl font-extrabold">
                                 76
                             </p>
@@ -31,7 +31,9 @@ export default function Home() {
             </div>
 
             {/* Bottom section */}
-            {/* TODO */}
+            <div className="h-5/9 text-lg font-semibold">
+                Summary
+            </div>
         </div>
     );
 }
